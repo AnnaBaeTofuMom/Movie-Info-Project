53cfacacc46221625daeec0188dcbe0c780c981e
TofuMom <mymininic@gmail,com>
2021-10-29 02:57:21 +0900

    preFetchData 적용해서 스크롤로 데이터 불러오기

--- SeSacDay13Project/SearchViewController.swift
+++ SeSacDay13Project/SearchViewController.swift
@@ -13,23 +13,21 @@ import Kingfisher
 
 class SearchViewController: UIViewController, UITableViewDataSourcePrefetching {
     
-    /*셀이 화면에 보이기 전에 필요한 리소스를 미리 다운받는 기능
+    
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths {
             if movieData.count - 1 == indexPath.row {
                 startPage += 10
-                fetchMovieData()
+                fetchMovieData(query: searchBar.text ?? "")
+                print("무비데이터 개수 : \(movieData.count)")
             }
         }
-    } */
+    }
     
     //취소
     
     var movieData: [MovieModel] = []
     
-    func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
-        print("cancel")
-    }
     
     
     var startPage = 1
@@ -44,7 +42,6 @@ class SearchViewController: UIViewController, UITableViewDataSourcePrefetching {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        fetchMovieData()
         
         
         
@@ -59,13 +56,15 @@ class SearchViewController: UIViewController, UITableViewDataSourcePrefetching {
         searchTableView.prefetchDataSource = self
         searchTableView.dataSource = self
         
+        searchBar.delegate = self
+        
         
         
     }
     
-    func fetchMovieData() {
+    func fetchMovieData(query: String) {
         
-        if let query = "스파이더맨".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
+        if let query = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) {
             let url = "https://openapi.naver.com/v1/search/movie.json?query=\(query)&display=10&start=1"
             
             let header: HTTPHeaders = [
@@ -78,7 +77,7 @@ class SearchViewController: UIViewController, UITableViewDataSourcePrefetching {
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
-                print("JSON: \(json)")
+                //print("JSON: \(json)")
                 
                 for item in json["items"].arrayValue {
                     
@@ -92,13 +91,13 @@ class SearchViewController: UIViewController, UITableViewDataSourcePrefetching {
                     let data = MovieModel(titleData: value, imageData: image, linkData: link, userRatingData: userRating, subtitle: sub)
                     
                     self.movieData.append(data)
-                    print(data)
+                    //print(data)
                     
                 }
                 //핵중요, 리로드 안해주면 아무것도 안뜨겠죠?
                 self.searchTableView.reloadData()
                 
-                print(self.movieData)
+                //print(self.movieData)
                 
             case .failure(let error):
                 print(error)
@@ -140,7 +139,36 @@ extension SearchViewController: UITableViewDataSource {
     }
     
 }
-
+extension SearchViewController: UISearchBarDelegate {
+    //검색버튼 눌렀을 떄 실행
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print(#function)
+        if let text = searchBar.text {
+            movieData.removeAll()
+            startPage = 1
+            fetchMovieData(query: text)
+        }
+        
+    }
+    //취소버튼 눌렀을 때 실행
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        print(#function)
+        movieData.removeAll()
+        searchTableView.reloadData()
+        searchBar.text = ""
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
+    //서치바에 커서가 깜빡거리기 시작할때 인식
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        print(#function)
+        searchBar.setShowsCancelButton(true, animated: true)
+    }
+    
+    //func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+     //
+    //}
+}
 
 extension SearchViewController: UITableViewDelegate {
     
